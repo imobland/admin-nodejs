52aa1ed5a37f966cd7758e909f5bb471c06ba810
Anderson D. Rosa <andersondrosa@outlook.com>
2020-07-17 17:26:09 -0300

    ..

--- nodejs.php
+++ nodejs.php
@@ -1,21 +1,20 @@
 <?php
-//
 class nodejs
 {
     public static $node = "node";
+    public static $path = "src";
 
     public static function run()
     {
         $node = self::$node;
+        $path = self::$path;
 
         $args = base64_encode(json_encode(func_get_args()));
       
-        $command = "$node src/index.js --base64 $args 2>&1";
+        $command = "$node $path/index.js --base64 $args 2>&1";
         
         exec($command, $lines);
 
         return $lines;
     }
 }
-
-nodejs::$node = "node";
